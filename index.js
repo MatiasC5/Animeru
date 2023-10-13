aa8ffbb645a288233f4f9ee898a94cd92f526d6d
MatiasC5 <cardosomatias1994@gmail.com>
2023-10-13 18:25:44 -0300

    cambio de api y estilos

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 const animeInfo = document.querySelector('#anime-info')
 
-
 const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'cd7649ce63msh2a0f91ab425013cp15287ejsnc08f338eed8c',
-    'X-RapidAPI-Host': 'animes5.p.rapidapi.com'
-  }
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cd7649ce63msh2a0f91ab425013cp15287ejsnc08f338eed8c',
+		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+	}
 };
-
 
 async function getAnime(e) {
   e.preventDefault()
   const { value } = e.target.anime
-  fetch(`https://animes5.p.rapidapi.com/?q=${value}&limit=15`, options)
+  const url = `https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=${value}&sortBy=ranking&sortOrder=asc`;
+  fetch(url, options)
     .then(response => response.json())
     .then(data => {
       showOnScreen(data)
+      console.log(data.data[0]);
     })
     .catch(err => console.error(err));
 }
 
-
 function showOnScreen(data) {
   animeInfo.textContent = ''
-  for (let i = 0; i < data.animes.length; i++) {
+  for (let i = 0; i < data.data.length; i++) {
     const animeContainer = document.createElement('div')
     animeContainer.classList.add('anime-container')
     const animeTitle = document.createElement('h2')
     const animeImage = document.createElement('img')
-    animeTitle.textContent = data.animes[i].title
-    animeImage.src = data.animes[i].main_picture.medium
+    animeTitle.textContent = data.data[i].title
+    animeImage.src = data.data[i].image
 
     animeContainer.append(animeTitle, animeImage)
     animeInfo.append(animeContainer)
